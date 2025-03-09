8acb4e7ccdffc60df43e27156857214eaea84bee
Amir Davtalab <amirhossindavtalab@gmail.com>
2025-03-09 22:14:24 +0330

    Fixed PostgreSQL connection settings for Railway

--- kdrama/settings.py
+++ kdrama/settings.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,16 +74,16 @@ TEMPLATES = [
     },
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quiz',
-        'USER': 'postgres',
-        'PASSWORD': 'AhDIU1981',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'quiz',
+#         'USER': 'postgres',
+#         'PASSWORD': 'AhDIU1981',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 WSGI_APPLICATION = 'kdrama.wsgi.application'
